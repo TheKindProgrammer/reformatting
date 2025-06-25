0ebd3ee6d16eb6ea62a78933388e3d8a5c145fa8
Zachary <zach@MacBook-Pro-184.local>
2025-06-25 17:10:53 -0400

    v1

---
+++ main.py
@@ -0,0 +1,26 @@
+def reformatNumber(self, number):
+    number = number.replace('-', '')
+    number = number.replace(' ', '')
+
+    lst = []
+    while len(number) > 4:
+        three_digits = number[0:3]
+        lst.append(three_digits)
+        lst.append('-')
+        number = number[3:]
+
+    if len(number) == 2:
+        lst.append(number)
+
+    if len(number) == 3:
+        lst.append(number)
+
+    if len(number) == 4:
+        two_digits = number[0:2]
+        lst.append(two_digits)
+        lst.append('-')
+        number = number[2:]
+        lst.append(number)
+
+    s = ''.join(lst)
+    return s
